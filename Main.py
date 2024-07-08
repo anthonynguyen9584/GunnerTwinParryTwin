b47a2f44f58fd77b4d2770b42fa7827471aef509
anthonynguyen9584 <121624906+anthonynguyen9584@users.noreply.github.com>
2024-07-08 01:29:10 -0400

    Add files via upload

---
+++ Main.py
@@ -0,0 +1,29 @@
+import pygame,src,asyncio
+from sys import exit
+
+pygame.init()
+screen = pygame.display.set_mode((800,600))
+pygame.display.set_caption("Gunner Twin Perry Twin") 
+
+
+async def main():
+
+    music = pygame.mixer.music.load("Assets/Sound/AdhesiveWombat-Night-Shade.ogg")
+    pygame.mixer.music.set_volume(0.5)
+    pygame.mixer.music.play()
+
+    states = {
+        "GAMEPLAY": src.Gameplay(),
+        "GAMEOVER" : src.GameOver(),
+        "MENU" : src.Menu(),
+        "PAUSE" : src.Pause(),
+        "CREDITS" : src.Credits()
+    }
+
+    game = src.Game(screen, states, "MENU")
+    await game.run()
+
+    pygame.quit()
+    exit()
+
+asyncio.run(main())
